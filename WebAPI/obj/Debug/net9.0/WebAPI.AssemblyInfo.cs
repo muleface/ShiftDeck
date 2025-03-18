cb130c7868e44c41a0759667ddd69b72e7a37818
Omer Cohen <omerco199@gmail.com>
2025-03-18 19:45:30 +0200

    changed main to contain progress

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18ceeb9dbbd93ccf18b54d231f2d42aec13a3dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edca77de710ada585d91a4f5015d3972b1568598")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
