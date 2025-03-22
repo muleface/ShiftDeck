90d0812836e2a98afdedba2ae5cfa250ba72cb99
Omer Cohen <omerco199@gmail.com>
2025-03-22 23:00:47 +0200

    fixed gitignore, merging should be easier henceforth

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+330b1587e679a0b2bb06c4c0260d31805de12322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc55b3d16c54599f942879920cda2fee69a4f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
