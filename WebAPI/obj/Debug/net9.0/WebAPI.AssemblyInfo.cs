fa26a2c7faf6dd2b590f1163647bda488a441e4b
Omer Cohen <omerco199@gmail.com>
2025-03-31 12:17:09 +0300

    fixing the internService naming fiasco

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a114fac4034c6a315b41786d5ec6d309469b7beb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa4d798c6b414e37435424aa50e65ac43572581")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
