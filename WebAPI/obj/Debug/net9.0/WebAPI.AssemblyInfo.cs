6e8ed995c299118f53336fd191e4d207ae9d6be6
Omer Cohen <omerco199@gmail.com>
2025-04-17 23:16:57 +0300

    recognized the issue as being a mismatch in databases resulting in lacking manager permissions

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ceffd52afc909745ba03fd4e9600aab60f88d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18111c9b084a8b1e05cfbbd0cd702cced870d46b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
