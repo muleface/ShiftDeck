9f77c1bec90a288b9876744269d45dd56d9038f0
Omer Cohen <omerco199@gmail.com>
2025-04-19 11:26:55 +0300

    adding a manager variable for shift management in calendar

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8ed995c299118f53336fd191e4d207ae9d6be6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e5ce2af638d5f318c273f1962d947b438973f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Calendar/ShiftManager.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import { AppContext } from '../AppContext';
+import { Intern, Shift, createFauxShift, fauxShift } from '../API_Services/Models';
+import shiftService from '../API_Services/shiftService';
+
+const ShiftManager = {
+
+    
+}
